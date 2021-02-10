1db4b6dfdd4f6dd8f98d20aa526408cf3a2e57c6
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-10 17:08:54 -0300

    Calcular El Área de un Triángulo
    Programa de Práctica en C# Principiante

---
+++ GrabarEjecutar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BASE, ALTURA, RESUL;
+            string linea;
+            Console.Write("DIGITE LA BASE :"); linea = Console.ReadLine();
+            BASE = double.Parse(linea);
+            Console.Write("DIGITE LA ALTURA:"); linea = Console.ReadLine();
+            ALTURA = double.Parse(linea);
+            RESUL = (BASE * ALTURA) / 2;
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:####.00}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:c}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:f}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:g}", RESUL));
+            Console.WriteLine();
+            Console.WriteLine("HOY ES: " + String.Format("Hoy es {0:F}", DateTime.Now));
+            Console.WriteLine("HOY ES: " + String.Format("Hoy es {0:dddd}{0:dd/MM/yyy}",
+           DateTime.Now));
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
